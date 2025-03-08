edbd5bb70588262b6439f436099d59d313666260
Halhadus <eneshakans45@proton.me>
2025-03-08 16:21:21 +0300

    Testing

--- setup.sh
+++ setup.sh
@@ -15,3 +15,4 @@ echo "__EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/10_libhybris.
 echo "HYBRIS_LD_LIBRARY_PATH=/system_ext/lib64:/product/lib64:/odm/lib64:/vendor/lib64/soundfx:/vendor/lib64/hw:/vendor/lib64:/system/lib64:/apex/com.android.runtime/lib64:/apex/com.android.runtime/lib64/bionic" >> /etc/environment
 
 systemctl enable droidian-perf brightness flashlight-sysfs
+systemctl mask serial-getty@hvc0.service

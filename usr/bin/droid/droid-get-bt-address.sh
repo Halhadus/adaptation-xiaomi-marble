1af1522782e1ad5e68b910df6abb2ee706b44937
Halhadus <eneshakans45@proton.me>
2025-05-04 17:05:51 +0300

    Fixed bluetooth

--- usr/bin/droid/droid-get-bt-address.sh
+++ usr/bin/droid/droid-get-bt-address.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-cp $(/usr/bin/getprop ro.vendor.bt.bdaddr_path) /var/lib/bluetooth/board-address
+find /var/lib/bluetooth -maxdepth 1 -iname '*:*:*:*:*:*' | cut -d/ -f 5 > /var/lib/bluetooth/board-address
+chown root:root /var/lib/bluetooth/board-address
+chmod 644 /var/lib/bluetooth/board-address

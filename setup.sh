608aa1ec908b7c1bf5c746fa408b8d414427162e
Halhadus <eneshakans45@proton.me>
2025-05-04 17:07:39 +0300

    Update setup.sh after status changed to working

--- setup.sh
+++ setup.sh
@@ -14,5 +14,7 @@ cp -rf /adaptation-xiaomi-marble/* /
 echo "__EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/10_libhybris.json" >> /etc/environment
 echo "HYBRIS_LD_LIBRARY_PATH=/system_ext/lib64:/product/lib64:/odm/lib64:/vendor/lib64/soundfx:/vendor/lib64/hw:/vendor/lib64:/system/lib64:/apex/com.android.runtime/lib64:/apex/com.android.runtime/lib64/bionic" >> /etc/environment
 
-systemctl enable droidian-perf brightness flashlight-sysfs comple loopsys
+systemctl enable droidian-perf brightness flashlight-sysfs
+#systemctl enable loopsys comple
 systemctl mask serial-getty@hvc0.service
+systemctl mask droidian-fpd
